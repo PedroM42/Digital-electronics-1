0fc28834319f5a92a32ef3532cca9c489f4bbda1
Petr_Medek <MedekPetan@gmail.com>
2021-03-09 21:08:52 +0100

    hmm

--- Labs/04-segment/hax_7seg/hax_7seg.srcs/sources_1/new/top.vhd
+++ Labs/04-segment/hax_7seg/hax_7seg.srcs/sources_1/new/top.vhd
@@ -64,10 +64,39 @@ begin
               seg_o(1) => CF,
               seg_o(0) => CG
         );
-            
+       -- Connect one common anode to 3.3V     
        AN <= b"1111_0111";
-             
+       -- Display input value on LEDs      
        LED(3 downto 0) <= SW;      
-            
+       
+    -- LED(7:4) indicators
+    -- Turn LED(4) on if input value is equal to 0, ie "0000"
+    LED(4) <= '1' when (SW = "0000") else '0';
+    
+    -- Turn LED(5) on if input value is greater than "1001", ie 9
+    LED(5) <= '1' when (SW = "1010") else '0',
+              '1' when (SW = "1011") else '0',
+              '1' when (SW = "1100") else '0',
+              '1' when (SW = "1101") else '0',
+              '1' when (SW = "1110") else '0',
+              '1' when (SW = "1111") else '0';
+    
+    -- Turn LED(6) on if input value is odd, ie 1, 3, 5, ...
+    LED(6) <= '1' when (SW = "0001") else '0',
+              '1' when (SW = "0011") else '0',
+              '1' when (SW = "0101") else '0',
+              '1' when (SW = "0111") else '0',
+              '1' when (SW = "1001") else '0',
+              '1' when (SW = "1011") else '0',
+              '1' when (SW = "1101") else '0',
+              '1' when (SW = "1111") else '0';
+    
+    -- Turn LED(7) on if input value is a power of two, ie 1, 2, 4, or 8
+    LED(7) <= '1' when (SW = "0001") else '0',
+              '1' when (SW = "0010") else '0',
+              '1' when (SW = "0100") else '0',
+              '1' when (SW = "1000") else '0';
+             
+end architecture Behavioral;
 
-end Behavioral;
+
